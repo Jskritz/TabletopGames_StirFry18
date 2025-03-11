a5d5966fb88a46446699acc244f14cde1ae7066b
Joshua Kritz <joshuakritz2@gmail.com>
2025-03-11 11:36:01 +0000

    updates

--- src/main/java/games/stirfry18/gui/SF18DeckView.java
+++ src/main/java/games/stirfry18/gui/SF18DeckView.java
@@ -1,6 +1,7 @@
 package games.stirfry18.gui;
 
 import core.components.Deck;
+import games.stirfry18.components.IngredientCard;
 import games.sushigo.cards.SGCard;
 import gui.views.CardView;
 import gui.views.DeckView;
@@ -11,33 +12,33 @@ import java.awt.*;
 import static games.stirfry18.gui.SF18GUIManager.SGCardHeight;
 import static games.stirfry18.gui.SF18GUIManager.SGCardWidth;
 
-public class SF18DeckView extends DeckView<SGCard> {
+public class SF18DeckView extends DeckView<IngredientCard> {
 
     String dataPath;
     Image backOfCard;
 
-    public SF18DeckView(int player, Deck<SGCard> d, boolean visible, String dataPath, Rectangle rect) {
+    public SF18DeckView(int player, Deck<IngredientCard> d, boolean visible, String dataPath, Rectangle rect) {
         super(player, d, visible, SGCardWidth, SGCardHeight, rect);
         this.dataPath = dataPath;
         backOfCard = ImageIO.GetInstance().getImage(dataPath + "CardBack.png");
     }
 
     @Override
-    public void drawComponent(Graphics2D g, Rectangle rect, SGCard card, boolean front) {
+    public void drawComponent(Graphics2D g, Rectangle rect, IngredientCard card, boolean front) {
         Image cardFace = getCardImage(card);
         int fontSize = g.getFont().getSize();
         CardView.drawCard(g, new Rectangle(rect.x, rect.y, rect.width, rect.height-fontSize), card, cardFace, backOfCard, front);
         if (front) {
-            g.drawString(card.type.name(), rect.x + 2, (int) (rect.y + rect.height - fontSize * 1.5));
+            g.drawString(card.getCardType().name(), rect.x + 2, (int) (rect.y + rect.height - fontSize * 1.5));
         }
     }
 
-    private Image getCardImage(SGCard card)
+    private Image getCardImage(IngredientCard card)
     {
-        String cardName = card.type.name().toLowerCase();
-        if (card.type == SGCard.SGCardType.Maki) {
-            cardName = cardName + "_" + card.count;
-        }
+        String cardName = card.getCardType().getName().toLowerCase();
+//        if (card.type == SGCard.SGCardType.Maki) {
+//            cardName = cardName + "_" + card.count;
+//        }
         return ImageIO.GetInstance().getImage(dataPath + cardName + ".png");
     }
 
